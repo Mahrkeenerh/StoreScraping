4b35d2385c219aad1387562b4231c35a68f3dce1
Mahrkeenerh <samuelbuban@gmail.com>
2024-10-07 10:21:42 +0200

    Add language filtering

--- filter_dataset.py
+++ filter_dataset.py
@@ -1,9 +1,15 @@
 import json
 import os
+from tqdm import tqdm
 
-dataset_path = r"D:\Datasets\Games"
+import spacy
+from spacy.language import Language
+from spacy_langdetect import LanguageDetector
 
-with open(os.path.join(dataset_path, "apps.json"), encoding="utf-8") as f:
+
+dataset_path = r"/home/xbuban1/Games"
+
+with open(os.path.join(dataset_path, "apps_filtered.json"), encoding="utf-8") as f:
     apps = json.load(f)
 
 
@@ -20,7 +26,7 @@ def filter(downloads, rating):
 
     filtered_apps = []
 
-    for app in apps:
+    for app in tqdm(apps, desc="Filtering Rating"):
         try:
             if app["score"] is not None and app["score"] > rating:
                 filtered_apps.append(app)
@@ -29,9 +35,35 @@ def filter(downloads, rating):
 
     apps = filtered_apps
 
+    print(f"Filtered {len(apps)} apps")
+
+
+def filter_english():
+    global apps
+
+    def get_lang_detector(nlp, name):
+        return LanguageDetector()
+
+    nlp = spacy.load("en_core_web_sm")
+    Language.factory("language_detector", func=get_lang_detector)
+    nlp.add_pipe('language_detector', last=True)
+
+    filtered_apps = []
+
+    for app in tqdm(apps, desc="Filtering English"):
+        doc = nlp(app["description"])
+
+        if doc._.language["language"] == "en":
+            filtered_apps.append(app)
+
+    apps = filtered_apps
+
+    print(f"Filtered {len(apps)} apps")
+
 
 if __name__ == "__main__":
-    filter(10000, 4)
-    
-    with open(os.path.join(dataset_path, "apps_filtered.json"), "w", encoding="utf-8") as f:
+    # filter(500000, 4.4)
+    filter_english()
+
+    with open(os.path.join(dataset_path, "apps_filtered_en.json"), "w", encoding="utf-8") as f:
         json.dump(apps, f, indent=4, sort_keys=True, ensure_ascii=False)
